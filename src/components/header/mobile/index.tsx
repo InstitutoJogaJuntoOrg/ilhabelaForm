a6da8d215401f0a1b838d1c29a452bbb10bfe0ed
Ryan Oliveira <97217108+ryanoliveiragit@users.noreply.github.com>
2023-09-14 13:21:46 -0300

    fix/mobile header login

--- src/components/header/mobile/index.tsx
+++ src/components/header/mobile/index.tsx
@@ -1,28 +1,43 @@
+import { useEffect, useState } from "react";
 import MyDropdown from "../profile";
 import { StyledNavLink } from "../styles";
 import { MobileMenuContainer } from "./styles";
 
+const MobileMenu = ({ closeMenu }: any) => {
+  const [user, setUser] = useState<string | undefined>(undefined);
 
-const MobileMenu = ({closeMenu}: any) => {
-  const user = localStorage.getItem('username');
+  useEffect(() => {
+    const checkUserAuthentication = () => {
+      const emailFromLocalStorage = localStorage.getItem("username");
+
+      if (emailFromLocalStorage) {
+        setUser(emailFromLocalStorage);
+      }
+    };
+
+    checkUserAuthentication();
+  }, []);
 
   return (
-
-
-        <MobileMenuContainer>
-        <StyledNavLink to={"/"} onClick={closeMenu}>
-          Saiba mais
-        </StyledNavLink>
-        
-        <StyledNavLink to={user?"/inscricao":"/auth"} onClick={closeMenu}>
+    <MobileMenuContainer>
+      <StyledNavLink to={"/"} onClick={closeMenu}>
+        Saiba mais
+      </StyledNavLink>
+      <div>
+        {!user && <StyledNavLink to="/login">Fazer login</StyledNavLink>}
+        <div>
         {user && <MyDropdown />}
-        </StyledNavLink>
-        {!user && <MyDropdown/>}
-        <StyledNavLink to={"/inscricao"} onClick={closeMenu}>
-          Sobre
-        </StyledNavLink>
-      </MobileMenuContainer>
-    );
-  };
-  
-  export default MobileMenu;
+        </div>
+        <br />
+        <br />
+        {user && (
+          <StyledNavLink to={user ? "/inscricao" : "/auth"} onClick={closeMenu}>
+            Inscrição
+          </StyledNavLink>
+        )}
+      </div>
+    </MobileMenuContainer>
+  );
+};
+
+export default MobileMenu;
