82e5ae5e4edf505ddff6aa59855ef32b66a8d886
Ryan Oliveira <97217108+ryanoliveiragit@users.noreply.github.com>
2023-09-15 16:05:28 -0300

    update text

--- src/utils/cardone.tsx
+++ src/utils/cardone.tsx
@@ -2,12 +2,11 @@ export const cardOne = [
     {
       title: "Ilhabela Tech",
       description:
-        "Ilhabela Tech é um programa da Prefeitura de Ilhabela em parceria com a ONG Instituto Joga Junto que visa capacitar e incluir, prioritariamente, jovens de baixa renda, entre 17 e 24 anos, residentes na Ilhabela, beneficiários do CadÚnico, provenientes de escola pública, no universo da tecnologia.",
-      startDate: "17/10",
-      modalidade: 'Presencial, seg-qui',
-      periods: ["Tarde (14h às 17h)", "Noite (18:30 às 21:30)"],
-      modality: "Presencial",
-      location: "IETEC - Laboratório de informática, Av. Profº Malaquias de O. Freitas, 448 - Barra Velha",
+        "Ilhabela Tech é um programa da Prefeitura de Ilhabela em parceria com a ONG Instituto Joga Junto que visa capacitar e incluir, prioritariamente, jovens de baixa renda, entre 17 e 24 anos, residentes na Ilhabela, provenientes de escola pública, no universo da tecnologia.",
+      startDate: " 17/10",
+      modalidade: 'Presencial',
+      periods: ["Tarde das 14h às 17h ou noite das 18:30 às 21:30  "],
+      modality: "Presencial, na IETC de Ilhabela de segunda à quinta",
+      location: "IETEC Laboratório de informática Av. Profº Malaquias de O. Freitas, 448 - Barra Velha",
     },
   ];
-  
