a2652028f7e921fa4cbc0726c4297443f3ca19e8
Ryan Oliveira <97217108+ryanoliveiragit@users.noreply.github.com>
2023-09-18 19:42:26 -0300

    children

--- src/pages/form/components/options/childrens.ts
+++ src/pages/form/components/options/childrens.ts
@@ -2,8 +2,8 @@ export const childrens = [
   { name: 'Nenhum', value: '0' },
   { name: '1', value: '1' },
   { name: '2', value: '2' },
-  { name: '3', value: '3' },
-  { name: '4', value: '4' },
-  { name: '5', value: '5' },
-  { name: '6', value: '6' },
+  { name: '3', value: '2' },
+  { name: '4', value: '2' },
+  { name: '5', value: '2' },
+  { name: '6', value: '2' },
 ];
