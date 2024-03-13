58650be96d66ab11778d8c0cade8ce7df6030a81
mgeambastiane <mgeambastiane@gmail.com>
2024-03-13 18:00:22 -0300

    Feature: senha reset

--- src/pages/auth/password/confirm.tsx
+++ src/pages/auth/password/confirm.tsx
@@ -99,18 +99,13 @@ export const NewPassword = () => {
             params[key] = decodeURIComponent(value); // decodifica os valores dos parâmetros
         }
 
+        setUuid(params.uid);
+        setToken(params.token);
         return params;
     }
 
     const params = getUrlParams();
-    if (params.uid) {
-        setUuid(params.uid);
-        console.log(params.uid)
-
-    }
-    if (params.token) {
-        setToken(params.token);
-    }
+    
 }, []);
 
   console.log(errors);
