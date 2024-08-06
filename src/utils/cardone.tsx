8fa147d7a686cf513ac416c200633c80f5a982d6
Ryan Oliveira Brito <ryanoliveirabrito178@gmail.com>
2024-08-06 12:49:39 -0300

    fix horario

--- src/utils/cardone.tsx
+++ src/utils/cardone.tsx
@@ -5,7 +5,7 @@ export const cardOne = [
       "É um programa da Prefeitura de Ilhabela em parceria com o Instituto Joga Junto que visa capacitar e incluir novos profissionais no mercado tecnológico por meio do conhecimento técnico de testador de Qualidade de Sofware (Q.A)",
     startDate: "09/09",
     modalidade: "Presencial",
-    periods: ["Turmas noturnas das 18:30 às 21:30"],
+    periods: ["Turmas noturnas das 19:00 às 21:30"],
     modality: "Presencial, na IETC de Ilhabela de segunda à quinta",
     location:
       "IETEC Laboratório de informática Av. Profº Malaquias de O. Freitas, 448 - Barra Velha",
