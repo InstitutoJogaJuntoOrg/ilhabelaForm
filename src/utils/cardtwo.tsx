28d3f8a8c1f1738f14c091958a3932f9c7b70a57
ryanoliveiragit <ryanoliveirasp@gmail.com>
2025-10-29 14:33:27 -0300

    ux course

--- src/utils/cardtwo.tsx
+++ src/utils/cardtwo.tsx
@@ -1,9 +1,9 @@
 export const cardTwo = [
   {
-    Programa: "O que é o curso de QA?",
+    Programa: "O que é o curso de UX?",
     Fases: [
       {
-        Nome: "O curso de Quality Assurance do Instituto Joga Junto foi desenhado para formar profissionais completos, que aliam competências técnicas a habilidades comportamentais, fundamentais para o trabalho em equipe, pensamento crítico, comunicação e adaptabilidade. Utilizando metodologias ativas, storytelling, gamificação e desafios reais, o curso proporciona uma formação dinâmica, conectada à realidade do mercado e às demandas das equipes de desenvolvimento ágil.",
+        Nome: "User Experience, Experiência do Usuário ou simplesmente UX É a área que cria experiências digitais que fazem sentido para as pessoas. Quem atua nesta profissão entende o comportamento dos usuários, identifica suas necessidades e desenha soluções fáceis e agradáveis de usar. É a forma como uma pessoa percebe, sente e interage com um produto, serviço ou sistema — especialmente no ambiente digital, como sites, aplicativos ou plataformas."
       },
 
     ],
